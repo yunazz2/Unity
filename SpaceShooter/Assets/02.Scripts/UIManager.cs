b0537336173a0cf7939f79081ea7c618b5de3ead
이유나 <z____yn@naver.com>
2024-06-05 17:12:37 +0900

    ☃️

---
+++ SpaceShooter/Assets/02.Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+// UI 버튼 이벤트 처리
+public class UIManager : MonoBehaviour
+{
+    public Button startButton;
+    public Button optionButton;
+    public Button shopButton;
+
+    private UnityAction action;
+
+    private void Start()
+    {
+        // UnityAction을 사용한 이벤트 연결 방식
+        action = () => OnButtonClick(startButton.name);
+        startButton.onClick.AddListener(action);
+
+        // 무명 메소드를 활용한 이벤트 연결 방식
+        optionButton.onClick.AddListener(delegate{OnButtonClick(optionButton.name);});
+
+        // 람다식을 활용한 이벤트 연결 방식
+        shopButton.onClick.AddListener(() => OnButtonClick(shopButton.name));
+    }
+    public void OnButtonClick(string msg)
+    {
+        Debug.Log(msg);
+    }
+}
